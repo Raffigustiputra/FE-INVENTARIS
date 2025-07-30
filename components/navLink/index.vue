890957759ba7df13f13367dea25418241ecd9117
AzrielDanu <jiellowkey777@gmail.com>
2025-07-30 19:49:13 +0700

    secong push

--- components/navLink/index.vue
+++ components/navLink/index.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="mx-4 my-4">
-        <div class="px-4 py-2 text-center rounded-lg bg-red-400 hover:bg-black duration-300 hover:cursor-pointer">
-            <h1>{{ props.navigationItem }}</h1>
+        <div class="px-4 py-2  rounded-lg flex items-center gap-3 hover:bg-black/20 duration-300 hover:cursor-pointer">
+            <div class="size-6 bg-black/10 rounded-full ">
+                
+            </div>
+            <h1 class="text-sm font-semibold">{{ props.navigationItem }}</h1>
         </div>
         <!-- <div class="px-4 py-2 text-center rounded-lg bg-red-400 hover:bg-black duration-300 hover:cursor-pointer">
             <h1>Main Menu</h1>
--- components/sidebarNav/index.vue
+++ components/sidebarNav/index.vue
@@ -1,8 +1,18 @@
 <template>
     <div class="">
-        <div class="w-2/12 fixed h-screen bg-blue-950">
-            <NavLink navigationItem = "Main Menu"/>
-            <NavLink navigationItem = "Main Menu"/>
+        <div class="w-2/12 fixed h-screen bg-gray-100">
+            <div class="flex items-center my-6 px-4">
+                <div class="size-8 bg-black">
+                    
+                </div>
+                <h1 class="text-2xl font-bold px-2">SIMPB</h1>
+            </div>
+            <div class="h-0.5 bg-black/10 mx-4">
+            </div>
+            <div>
+                <NavLink navigationItem = "Main Menu"/>
+                <NavLink navigationItem = "Main Menu"/>
+            </div>
         </div>
     </div>
 </template>
