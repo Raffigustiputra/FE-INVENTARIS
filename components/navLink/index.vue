ea7d505ea19c1edb532eb8f5b8100ec079fb3148
AzrielDanu <jiellowkey777@gmail.com>
2025-07-30 17:38:43 +0700

    pull zan

--- components/navLink/index.vue
+++ components/navLink/index.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>
-        <h1>titel</h1>
-    </div>
+    <div class="mx-4 my-4">
+        <div class="px-4 py-2 text-center rounded-lg bg-red-400 hover:bg-black duration-300 hover:cursor-pointer">
+            <h1>titel</h1>
+        </div>
+        </div>
 </template>
