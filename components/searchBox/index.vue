5a3bbe4a1b21f50bb71c7a315c650eac02f4fcc7
nurrizky <nurrizkyr90@gmail.com>
2025-07-31 22:30:43 +0700

    component search box

---
+++ components/searchBox/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="w-[243] h-[36px] p-2 border-2 border-[#E0E0E0] rounded-md flex items-center gap-2"
+  >
+    <IconsSearchIcon class="w-6 h-6 text-gray-500" />
+    <input
+      type="text"
+      placeholder="Search bla bla bla..."
+      class="outline-none w-full"
+    />
+  </div>
+</template>
+<script setup>
+import { IconsSearchIcon } from "#components";
+</script>
