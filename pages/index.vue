a2777f9bf33f6cd75cd9692a68e206db504ac69a
Pandu-Pratama-08 <panduputrapratama@smkwikrama.sch.id>
2025-08-07 20:14:22 +0700

    update login enter

--- pages/index.vue
+++ pages/index.vue
@@ -52,10 +52,10 @@ const showAlert = (type, message) => {
         alert(message);
     }
 };
-
+    
 const login = async () => {
     try {
-        const response = await $fetch(`${url}/login`, {
+        const response = await $fetch(`${url}/api/login`, {
             method: 'POST',
             body: {
                 username: authStore.input.username,
@@ -122,7 +122,7 @@ definePageMeta({
                 </div>
             </div>
 
-            <div class="w-5/12 pt-4 pb-8 rounded-xl bg-white">
+            <form @submit.prevent="login" class="w-5/12 pt-4 pb-8 rounded-xl bg-white">
                 <div class="w-full text-center">
                     <h1 class="font-semibold">Log in to start using WikVentory</h1>
                 </div>
@@ -184,7 +184,7 @@ definePageMeta({
                         LOGIN
                     </button>
                 </div>
-            </div>
+            </form>
          <!--copy right-->
             <p class="text-xs mt-10">
                 © PPLG XII-V 2025. All Rights Reservedd
