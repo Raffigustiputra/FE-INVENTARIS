36bc96800a2f8ed5b28fb4f43f95a726d9c28175
AzrielDanu <jiellowkey777@gmail.com>
2025-08-10 21:16:13 +0700

    add: crud in accounts

---
+++ stores/accounts.ts
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+import Accounts from "~/components/icons/accounts.vue";
+
+export const useAccountsStore = defineStore("accounts", {
+    state: () => ({
+        input : {
+            id : '',
+            name : '',
+            username : '',
+            role : '',
+            password : '',
+            major_id : '',
+        },
+        Accounts : [],
+    }),
+});
