8994aa33cfd2073ef0823d808bd4ad0449e448fb
achmadwhildan <achmad.whildan@gmail.com>
2025-08-19 21:28:00 +0700

    middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,25 +2,38 @@ import { useAuthStore } from "~/stores/auth";
 
 export default defineNuxtRouteMiddleware((to, from) => {
   const authStore = useAuthStore();
+
   // Kalau belum login
   if (!authStore.getToken) {
     if (to.path !== "/") {
-      return navigateTo("/");
+      return navigateTo({
+        path: "/",
+        query: { message: "You must login first" }
+      });
     }
   }
 
   // superadmin → akses /admin/*
   if (to.path.startsWith("/admin") && authStore.role !== "superadmin") {
-    return navigateTo("/");
+    return navigateTo({
+      path: "/",
+      query: { message: "No access to Admin Page" }
+    });
   }
 
   // admin → akses /kaprog/*
   if (to.path.startsWith("/kaprog") && authStore.role !== "admin") {
-    return navigateTo("/");
+    return navigateTo({
+      path: "/",
+      query: { message: "No access to Kaprog Page" }
+    });
   }
 
   // user → akses /user/*
   if (to.path.startsWith("/user") && authStore.role !== "user") {
-    return navigateTo("/");
+    return navigateTo({
+      path: "/",
+      query: { message: "No access to User Page" }
+    });
   }
 });
