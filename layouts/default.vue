d78194782cc6b3aaa7e4d4a49216dd6c13ad8655
rafif2112 <m.rafif887@gmail.com>
2025-08-27 22:45:58 +0700

    refactor layout structure and improve code readability in default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,40 +4,47 @@
   </Head>
   <div class="w-full min-h-screen font-Poppins">
     <!-- Online checking -->
-    <div v-if="!isOnline" class="h-screen flex flex-col text-center font-Poppins">
+    <div
+      v-if="!isOnline"
+      class="h-screen flex flex-col text-center font-Poppins"
+    >
       <header>
         <img
           src="/public/images/WV-BLACKLANDSCAPE.png"
           alt="WV black landscape"
-          class="w-50 m-5"/>
+          class="w-50 m-5"
+        />
       </header>
-      <main class="text-black h-[80%] flex flex-col items-center justify-center">
+      <main
+        class="text-black h-[80%] flex flex-col items-center justify-center"
+      >
         <img
           src="/public/images/errorImages/403.png"
           alt="404 Image"
-          class="w-90"/>
-        <div
-          class="flex items-center flex-col">
+          class="w-90"
+        />
+        <div class="flex items-center flex-col">
           <h2 class="text-3xl font-semibold text-gray-800">Error Connection</h2>
           <p class="text-sm text-gray-400 font-medium mt-4">
-            Your device may be offline or the WiKVENTORY <br> server may be experiencing problems.
+            Your device may be offline or the WiKVENTORY <br />
+            server may be experiencing problems.
           </p>
         </div>
       </main>
     </div>
 
-    
     <div v-else class="flex">
-      <div v-if="loading" class="flex fixed inset-0 items-center justify-center z-50 bg-black/20">
-         <Loading/>
+      <div
+        v-if="loading"
+        class="flex fixed inset-0 items-center justify-center z-50 bg-black/20"
+      >
+        <Loading />
       </div>
       <div class="w-2/12">
-        <SidebarNav 
-          @logout="handleLogout"
-        />
+        <SidebarNav @logout="handleLogout" />
       </div>
       <div class="">
-        <Navbar/>
+        <Navbar />
       </div>
       <div class="ml-12 w-8/10 flex py-4 px-10 flex-col">
         <div class="w-full mt-20 h-auto">
@@ -45,15 +52,14 @@
         </div>
       </div>
     </div>
-
   </div>
 </template>
 <script setup>
-import Loading from '@/components/loading/index.vue'
-const { isOnline } = useOnlineStatus()
+import Loading from "@/components/loading/index.vue";
+const { isOnline } = useOnlineStatus();
 const route = useRoute();
 
-const loading = ref(true);
+const loading = ref(false);
 
 function handleLogout(val) {
   loading.value = val;
