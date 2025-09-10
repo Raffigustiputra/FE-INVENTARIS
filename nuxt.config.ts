40a4f42ea69f3b98d83da5ceb12703e4f50275b4
M-Bayu-Aji <muhammadbayuajisutisnapurta@gmail.com>
2025-09-10 20:33:42 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,10 +5,10 @@
      devtools: { enabled: true },
      css: ["~/assets/css/main.css"],
 
-     devServer: {
-       host: "15.15.15.146",
-       port: 5173, // bisa diganti
-     },
+    //  devServer: {
+    //    host: "15.15.15.146",
+    //    port: 5173, // bisa diganti
+    //  },
 
      vite: {
        plugins: [tailwindcss()],
